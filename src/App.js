2b780a6afa7464ba89df877f25cf32115666bcb6
unknown <sitaiphan@gmail.com>
2022-07-11 20:46:38 +0700

    td

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import './App.css';
-import Bai2 from './components/Bai2';
-import Bai3 from './components/Bai3';
-import Menu from './components/menu';
+import Bai2 from './homeword-ss2/Bai2';
+import Bai3 from './homeword-ss2/Bai3';
+import Menu from './homeword-ss2/menu';
  
 
 function App() {
@@ -10,10 +10,6 @@ function App() {
     <Menu color='red' size={20}></Menu>
     <Bai2></Bai2>
     <Bai3></Bai3>
-
-
-      {/* <JSX></JSX> */}
-      {/* <ES6></ES6> */}
     </>
   );
 }
